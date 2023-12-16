33b533d8999cc4c197dbef00d6d10698ecdc1f04
jacobsweaver96 email = jacobsweaver@gmail.com <jacobsweaver@gmail.com>
2023-12-16 15:13:57 -0600

    Added catalogitem

---
+++ Portfolio.Catalog.Models/Models/CatalogItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Portfolio.Catalog.Models.Models
+{
+    public class CatalogItem
+    {
+        /// <summary>
+        /// Identifier
+        /// </summary>
+        public required string Id { get; set; }
+
+        /// <summary>
+        /// Name of the item
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Description of the item
+        /// </summary>
+        public required string Description { get; set; }
+
+        /// <summary>
+        /// Volume of this item (not always required)
+        /// </summary>
+        public int? Count { get; set; }
+    }
+}
